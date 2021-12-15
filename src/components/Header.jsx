07db583ea9e83f0272530e3e7e00dcca83c27fb6
thounny <thounnykeo@gmail.com>
2021-12-15 06:32:26 -0500

    Imported NightsStayIcon @mui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import NightsStayIcon from '@material-ui/icons/NightsStay';
 function Header() { 
     return (
     <header>
-    <h1>Note App</h1>
+<h1>
+<NightsStayIcon />
+Note App
+</h1>
     </header>
     );
 }
